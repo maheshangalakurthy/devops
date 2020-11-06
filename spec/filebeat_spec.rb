5218692bdaee593ba2df1adb69aa3efc792a5018
maheshangalakurthy <angalakurthy.mahesh@gmail.com>
2020-11-06 17:56:37 +0530

    inspec test

---
+++ spec/filebeat_spec.rb
@@ -0,0 +1,70 @@
+require "docker"
+require "docker-api"
+require "serverspec"
+
+describe 'Dockerfile' do
+    before(:all) do
+        image = Docker::Image::build_from_dir('.')
+        #@container = image.run()
+        set :os, family: :redhat
+        set :backend, :docker
+        set :docker_image, image.id
+        #set :Docker_container, @container.id
+    end
+
+    describe file('/etc/os-release') do
+        it { should be_file }
+    end
+    
+
+    describe user('filebeat') do
+        it { should exist }
+        it { should belong_to_group 'filebeat' }
+        it { should belong_to_primary_group 'filebeat' }
+        it { should have_uid 1000 }
+        it { should have_home_directory '/home/filebeat' }
+        it { should have_login_shell '/bin/bash' }
+    end
+    
+    describe group('filebeat') do
+        it { should exist }
+        it { should have_gid 1000 }
+    end
+
+    describe file('/usr/share/filebeat') do
+        it { should be_directory }
+        it { should exist }
+        it { should be_owned_by 'filebeat' }
+        it { should be_grouped_into 'filebeat' }
+    end
+
+    describe file('/usr/share/filebeat/bin/filebeat') do
+        it { should be_file }
+        it { should be_owned_by 'filebeat' }
+        it { should be_grouped_into 'filebeat' }
+    end
+
+    describe command('rpm -qa | grep filebeat') do
+        its(:stdout) { should match('filebeat-5.2.0-1.x86_64') }
+    end
+
+    describe 'Dockerfile#running' do
+        before(:all) do
+          @container = Docker::Container.create(
+            'Image'      => @image.id,
+            
+          )
+    
+          @container.start
+        end
+            
+        describe command('whoami') do
+            its(:stdout) { should match('filebeat') }
+        end
+            
+        after(:all) do
+          @container.kill
+          @container.delete(:force => true)
+        end
+    end
+end
